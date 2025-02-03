63be38b4d2e6398fb79b1e61225a19697cf13b40
BoRaT-7 <boratabid@gmail.com>
2025-02-03 23:35:12 +0600

    commit -1

--- max.js
+++ max.js
@@ -1,2 +1,15 @@
-var age = 32;
-console.log(age);
+const money  = 700.
+;
+
+if(money=>900){
+    console.log("Oper tikit");
+
+}
+else{
+    if(money<100)
+        console.log("tui gorib ami na ")
+    else{
+        if(money>0)
+            console.log("fokir")
+    }
+}
