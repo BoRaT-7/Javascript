58a0d3b32bcfde82c27e3a90ea7e42510a7dee15
BoRaT-7 <boratabid@gmail.com>
2025-02-08 23:22:47 +0600

    commit 1

--- max.js
+++ max.js
@@ -1,12 +1,8 @@
-var num1;
-num1 = 20;
-var num2 = 19;
-var result;
-
-if (num1 > num2) {
-    result = num1;
-    console.log("result will be num1");
-} else {
-    result = num1 + num2;
-    console.log("total =", result);
+const mobile = {
+    band:'samsumg',
+    price:2300,
+    color:'12px',
+}
+for (const prop in mobile){
+    console.log(prop)
 }
