c16318136219ecc9da9b9ccf1a6d1ea10134c9e4
BoRaT-7 <boratabid@gmail.com>
2025-02-15 21:24:14 +0600

    commit 3

--- javaprb.js
+++ javaprb.js
@@ -20,3 +20,21 @@ function oddaverage (number){
 const number = [6,8,2,1,7,4,3,9];
 const avg = oddaverage (number)
 console.log('the average is =',avg);
+
+
+
+// Problem remove from the array  Number 2............
+const goodboy = ['borat', 'abid','murad','nila','foysal','borat','nila'];
+const num = [1,2,4,6,7,1,4,88,99,33,2,77,]
+function nobudlicat (array){
+    const uni = [];
+    for(const iteam of array){
+        if(uni.includes(iteam)===false){
+            uni.push(iteam);
+        }
+    }
+    return uni;
+}
+const okyarray = nobudlicat(goodboy);
+console.log(okyarray);
+console.log(nobudlicat)
